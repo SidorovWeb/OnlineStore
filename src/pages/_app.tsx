85baa1c8de9244311f8ccdb325a7a8e798d2bba4
Alexandr <sidorovweb@gmail.com>
2023-09-26 23:41:27 +0300

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,9 @@ import Layout from '@/components/Layout/Layout'
 import Modal from '@/components/Modal/Modal'
 import Overlay from '@/components/Overlay/Overlay'
 import MasterProvider from '@/contex/MasterProvider'
-import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
