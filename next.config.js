f03b34851550116a507bc89c8b6f6e21bbd1aa69
Alexandr <sidorovweb@gmail.com>
2023-10-08 12:56:11 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
             'fakestoreapi.com',
             'lh3.googleusercontent.com',
             'sun1-28.userapi.com',
+            'sun6-22.userapi.com',
         ],
     },
 }
