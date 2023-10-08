6fbddc33195792978994866e5a4bf45c675b9ead
Alexandr <sidorovweb@gmail.com>
2023-10-09 01:55:49 +0300

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,15 @@ export default function Document() {
     return (
         <Html lang="ru">
             <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link
                     rel="preconnect"
-                    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;900&display=swap"
-                    // crossorigin=
+                    href="https://fonts.gstatic.com"
+                    crossOrigin=""
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;900&display=swap"
+                    rel="stylesheet"
                 />
             </Head>
             <body>
