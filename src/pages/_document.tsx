2d528efb6003c5e08cc469ed538fc25170734297
Alexandr <sidorovweb@gmail.com>
2023-10-08 14:09:34 +0300

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
             <Head>
                 <link
                     href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;900&display=swap"
-                    rel="stylesheet"
+                    rel="preconnect"
                 />
             </Head>
             <body>
