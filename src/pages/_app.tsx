43ca865deb86929fd3f4e7a460e6f86832dcd10a
Alexandr <sidorovweb@gmail.com>
2023-10-08 13:25:18 +0300

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,20 +3,18 @@ import Modal from '@/components/Modal/Modal'
 import Overlay from '@/components/Overlay/Overlay'
 import MasterProvider from '@/contex/MasterProvider'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 import '@/styles/globals.scss'
-import { Suspense } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Suspense>
+        <>
             <MasterProvider>
                 <Layout>
                     <Component {...pageProps} />
                 </Layout>
-                <Modal />
+                {/* <Modal /> */}
                 <Overlay />
             </MasterProvider>
-        </Suspense>
+        </>
     )
 }
