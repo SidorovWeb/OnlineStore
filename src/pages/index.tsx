84ce0d3528b663294176623c6ed34af1498d8806
Alexandr <sidorovweb@gmail.com>
2023-10-24 12:46:50 +0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,41 +5,32 @@ import { ICategoriesData } from '@/interfaces/categories.interface'
 import { IProductData } from '@/interfaces/products.interface'
 import { StoreService } from '@/services/store.service'
 import { GetStaticProps, NextPage } from 'next'
-import { useEffect, useState } from 'react'
 
 const HomePage: NextPage<IHomePageProps> = ({ categories, products }) => {
-    const [mounted, setMounted] = useState(false)
-    useEffect(() => {
-        setMounted(true)
-    }, [])
-    if (!mounted) {
-        return null // return this null to avoid hydration errors
-    }
-
     return (
         <>
-            <Banner />
+            {/* <Banner />
             <Trending products={products} title={'Trending'} />
-            <WorthSeeing categories={categories} />
+            <WorthSeeing categories={categories} /> */}
         </>
     )
 }
 
 export default HomePage
 
-export const getStaticProps: GetStaticProps = async () => {
-    const categories = await StoreService.getCategories()
-    const products = await StoreService.getProductsAll()
+// export const getStaticProps: GetStaticProps = async () => {
+//     const categories = await StoreService.getCategories()
+//     const products = await StoreService.getProductsAll()
 
-    if (!categories || !products) {
-        return {
-            notFound: true,
-        }
-    }
-    return {
-        props: { categories, products },
-        revalidate: 60,
-    }
-}
+//     if (!categories || !products) {
+//         return {
+//             notFound: true,
+//         }
+//     }
+//     return {
+//         props: { categories, products },
+//         revalidate: 60,
+//     }
+// }
 
 export type IHomePageProps = ICategoriesData & IProductData
