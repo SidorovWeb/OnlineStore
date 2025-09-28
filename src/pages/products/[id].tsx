f88a10abef378aba92b52dbef7426310fe941787
SidorovAleksandr <sidorovweb@gmail.com>
2025-09-28 19:22:16 +0300

    Fix categories

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,62 +1,69 @@
-import Trending from '@/components/Trending/Trending'
-import Product from '@/components/Product/Product'
-import { IProductData, IProductSingle } from '@/interfaces/products.interface'
-import { StoreService } from '@/services/store.service'
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
-import { ParsedUrlQuery } from 'querystring'
-import Head from 'next/head'
+import Trending from "@/components/Trending/Trending";
+import Product from "@/components/Product/Product";
+import {
+  IProduct,
+  IProductData,
+  IProductSingle,
+} from "@/interfaces/products.interface";
+import { StoreService } from "@/services/store.service";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { ParsedUrlQuery } from "querystring";
+import Head from "next/head";
 
 interface Params extends ParsedUrlQuery {
-    id: string
+  id: string;
 }
+
+interface IProductsPage extends IProductData, IProductSingle {}
 
 const ProductsPage: NextPage<IProductsPage> = ({ product, products }) => {
-    return (
-        <>
-            <Head>
-                <title>{product.title}</title>
-                <meta name="description" content={product.description} />
-                <meta property="og:title" content={product.title} />
-                <meta property="og:description" content={product.description} />
-                <meta property="og:type" content="article" />
-            </Head>
-            <Product product={product} />
-            <Trending products={products} title={'Related products'} />
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <title>{product.title}</title>
+        <meta name="description" content={product.description} />
+        <meta property="og:title" content={product.title} />
+        <meta property="og:description" content={product.description} />
+        <meta property="og:type" content="article" />
+      </Head>
+      <Product product={product} />
+      <Trending products={products} title={"Related products"} />
+    </>
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
 
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
-    const products = await StoreService.getProductsAll()
+  const products = await StoreService.getProductsAll();
 
+  return {
+    paths: products.map((p) => ({
+      params: {
+        id: String(p.id),
+      },
+    })),
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps<IProductsPage> = async ({
+  params,
+}) => {
+  const product = await StoreService.getByIdProduct(String(params?.id));
+  const allProducts = await StoreService.getProductsAll();
+  const products = allProducts
+    .filter((item) => item.category === product.category)
+    .filter((item) => item.id !== product.id);
+
+  if (!product || !products) {
     return {
-        paths: products.map((p) => ({
-            params: {
-                id: String(p.id),
-            },
-        })),
-        fallback: 'blocking',
-    }
-}
+      notFound: true,
+    };
+  }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const categories = await StoreService.getCategories()
-    const product = await StoreService.getByIdProduct(String(params?.id))
-    const products = await (await StoreService.getProductsAll())
-        .filter((item) => item.category === product.category)
-        .filter((item) => item.id !== product.id)
-
-    if (!categories || !product || !products) {
-        return {
-            notFound: true,
-        }
-    }
-    return {
-        props: { categories, product, products },
-        revalidate: 60,
-    }
-}
-
-type IProductsPage = IProductData & IProductSingle
+  return {
+    props: { product, products },
+    revalidate: 60,
+  };
+};
--- src/pages/products/categories/[id].tsx
+++ src/pages/products/categories/[id].tsx
@@ -1,172 +1,200 @@
-import Banner from '@/components/Banner/Banner'
-import style from './singleCategories.module.scss'
-import { IProduct, IProductData } from '@/interfaces/products.interface'
-import { StoreService } from '@/services/store.service'
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
-import { ParsedUrlQuery } from 'querystring'
-import Pagination, { paginate } from '@/components/Pagination/Pagination'
-import { useEffect, useState } from 'react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import Card from '@/components/Card/Card'
-import MyInput from '@/components/Input/MyInput'
-
-const CategoryPage: NextPage<IProductData> = ({ products }) => {
-    const { query } = useRouter()
-    const [currentPage, setCurrentPage] = useState(1)
-    const [pageSize, setPageSize] = useState(4)
-    const [filterByName, setFilterByName] = useState('')
-    const [filterByPrice, setFilterByPrice] = useState(0)
-    const [filter, setFilter] = useState<IProduct[]>([])
-    const [paginatedProduct, setPaginatedProduct] = useState<IProduct[]>([])
-
-    useEffect(() => {
-        setFilter([...products])
-        setPaginatedProduct(paginate([...products], 1, pageSize))
-    }, [products])
-
-    useEffect(() => {
-        if (query.id) {
-            setFilterByName('')
-            setFilterByPrice(0)
-            setCurrentPage(1)
-        }
-    }, [query.id])
-
-    useEffect(() => {
-        filteredProduct(filter)
-
-        if (filterByName === '' && filterByPrice === 0) {
-            setPaginatedProduct(paginate([...products], currentPage, pageSize))
-            setFilter([...products])
-        }
-
-        if (!filter.length) {
-            filteredProduct([...products])
-        }
-    }, [filterByName, filterByPrice])
-
-    const onPageChange = (page: number) => {
-        setCurrentPage(page)
-        setPaginatedProduct(paginate(filter, page, pageSize))
-    }
-
-    function filteredProduct(array: IProduct[], value?: string | number) {
-        const filteredProduct = array.filter((p) => {
-            const str = filterByName ?? value
-            const num = filterByPrice ?? value
-
-            return (
-                p.title.toLowerCase().includes(str.toLowerCase()) &&
-                p.price >= num
-            )
-        })
-
-        setFilter(filteredProduct)
-        setPaginatedProduct(paginate(filteredProduct, 1, pageSize))
-    }
-
-    const onChange = (value: string | number) => {
-        if (typeof value === 'string') setFilterByName(value)
-        if (typeof value === 'number') setFilterByPrice(value)
-
-        if (
-            typeof value === 'string' &&
-            filterByName.length > String(value).length
-        ) {
-            filteredProduct([...products], value)
-        }
-
-        if (typeof value === 'number' && value < filterByPrice) {
-            filteredProduct([...products], value)
-        }
-    }
-
-    return (
-        <>
-            <Banner />
-            <div className={style.container}>
-                <div className={style.inner}>
-                    <h2 className={style.header}>{query.id}</h2>
-                    <div className={style.filterLine}>
-                        <MyInput
-                            type={'search'}
-                            id={'1'}
-                            defaultValue={filterByName}
-                            onChange={(e) => onChange(e.target.value)}
-                            placeholder={'Product name'}
-                        />
-                        <MyInput
-                            type={'text'}
-                            id={'3'}
-                            defaultValue={filterByPrice}
-                            onChange={(e) => {
-                                if (isNaN(Number(e.target.value))) {
-                                    return false
-                                }
-                                onChange(Number(e.target.value))
-                            }}
-                            label={'Price from'}
-                            placeholder={'Price from'}
-                        />
-                    </div>
-                    <div className={style.list}>
-                        {paginatedProduct.length > 0 ? (
-                            paginatedProduct.map((products) => (
-                                <Link
-                                    href={`/products/${products.id}`}
-                                    className={style.card}
-                                    key={products.id}
-                                >
-                                    <Card product={products} />
-                                </Link>
-                            ))
-                        ) : (
-                            <p className={style.notFound}>Not found</p>
-                        )}
-                    </div>
-                    <Pagination
-                        items={filter.length}
-                        currentPage={currentPage}
-                        pageSize={pageSize}
-                        onPageChange={onPageChange}
-                    />
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default CategoryPage
+import Banner from "@/components/Banner/Banner";
+import style from "./singleCategories.module.scss";
+import { IProduct } from "@/interfaces/products.interface";
+import { StoreService } from "@/services/store.service";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { ParsedUrlQuery } from "querystring";
+import Pagination, { paginate } from "@/components/Pagination/Pagination";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Card from "@/components/Card/Card";
+import MyInput from "@/components/Input/MyInput";
 
 interface Params extends ParsedUrlQuery {
-    id: string
+  id: string;
 }
+
+interface ICategoryPageProps {
+  products: IProduct[];
+}
+
+const CategoryPage: NextPage<ICategoryPageProps> = ({ products }) => {
+  const { query } = useRouter();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(4);
+  const [filterByName, setFilterByName] = useState("");
+  const [filterByPrice, setFilterByPrice] = useState(0);
+  const [filter, setFilter] = useState<IProduct[]>([]);
+  const [paginatedProduct, setPaginatedProduct] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    setFilter([...products]);
+    setPaginatedProduct(paginate([...products], 1, pageSize));
+  }, [products]);
+
+  useEffect(() => {
+    if (query.id) {
+      setFilterByName("");
+      setFilterByPrice(0);
+      setCurrentPage(1);
+    }
+  }, [query.id]);
+
+  useEffect(() => {
+    filteredProduct(filter);
+
+    if (filterByName === "" && filterByPrice === 0) {
+      setPaginatedProduct(paginate([...products], currentPage, pageSize));
+      setFilter([...products]);
+    }
+
+    if (!filter.length) {
+      filteredProduct([...products]);
+    }
+  }, [filterByName, filterByPrice]);
+
+  const onPageChange = (page: number) => {
+    setCurrentPage(page);
+    setPaginatedProduct(paginate(filter, page, pageSize));
+  };
+
+  function filteredProduct(array: IProduct[], value?: string | number) {
+    const filteredProduct = array.filter((p) => {
+      const str = filterByName ?? value;
+      const num = filterByPrice ?? value;
+
+      return (
+        p.title.toLowerCase().includes(str.toLowerCase()) && p.price >= num
+      );
+    });
+
+    setFilter(filteredProduct);
+    setPaginatedProduct(paginate(filteredProduct, 1, pageSize));
+  }
+
+  const onChange = (value: string | number) => {
+    if (typeof value === "string") setFilterByName(value);
+    if (typeof value === "number") setFilterByPrice(value);
+
+    if (
+      typeof value === "string" &&
+      filterByName.length > String(value).length
+    ) {
+      filteredProduct([...products], value);
+    }
+
+    if (typeof value === "number" && value < filterByPrice) {
+      filteredProduct([...products], value);
+    }
+  };
+
+  return (
+    <>
+      <Banner />
+      <div className={style.container}>
+        <div className={style.inner}>
+          <h2 className={style.header}>{query.id}</h2>
+          <div className={style.filterLine}>
+            <MyInput
+              type={"search"}
+              id={"1"}
+              defaultValue={filterByName}
+              onChange={(e) => onChange(e.target.value)}
+              placeholder={"Product name"}
+            />
+            <MyInput
+              type={"text"}
+              id={"3"}
+              defaultValue={filterByPrice}
+              onChange={(e) => {
+                if (isNaN(Number(e.target.value))) {
+                  // Просто не обновляем состояние, если значение не число
+                  return;
+                }
+                onChange(Number(e.target.value));
+              }}
+              label={"Price from"}
+              placeholder={"Price from"}
+            />
+          </div>
+          <div className={style.list}>
+            {paginatedProduct.length > 0 ? (
+              paginatedProduct.map((product) => (
+                <Link
+                  href={`/products/${product.id}`}
+                  className={style.card}
+                  key={product.id}
+                >
+                  <Card product={product} />
+                </Link>
+              ))
+            ) : (
+              <p className={style.notFound}>Not found</p>
+            )}
+          </div>
+          <Pagination
+            items={filter.length}
+            currentPage={currentPage}
+            pageSize={pageSize}
+            onPageChange={onPageChange}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CategoryPage;
 
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
-    const products = await StoreService.getProductsAll()
+  const categoriesData = await StoreService.getCategories();
+
+  // Extract category strings from the nested structure
+  const categoryNames = categoriesData.flatMap(
+    (category) => category.categories
+  );
+
+  return {
+    paths: categoryNames.map((categoryName) => ({
+      params: {
+        id: categoryName,
+      },
+    })),
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps<
+  ICategoryPageProps,
+  Params
+> = async ({ params }) => {
+  if (!params?.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  try {
+    const productsData = await StoreService.getSpecificCategory(
+      String(params.id)
+    );
+
+    if (!productsData || productsData.length === 0) {
+      return {
+        notFound: true,
+      };
+    }
+
+    // Extract the actual products array from the IProductData structure
+    const products = productsData[0].products;
 
     return {
-        paths: products.map((p) => ({
-            params: {
-                id: String(p.id),
-            },
-        })),
-        fallback: 'blocking',
-    }
-}
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const products = await StoreService.getSpecificCategory(String(params?.id))
-    const categories = await StoreService.getCategories()
-
-    if (!categories || !products) {
-        return {
-            notFound: true,
-        }
-    }
-
+      props: { products },
+    };
+  } catch (error) {
     return {
-        props: { categories, products },
-    }
-}
+      notFound: true,
+    };
+  }
+};
