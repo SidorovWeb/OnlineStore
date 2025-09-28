10dabb5d0e09f6a2f38864c98ebef7f2ca0f3712
SidorovAleksandr <sidorovweb@gmail.com>
2025-09-28 18:44:18 +0300

    Update Node.js version to 22.x

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: [
-            'fakestoreapi.com',
-            'lh3.googleusercontent.com',
-            'sun1-28.userapi.com',
-            'sun6-22.userapi.com',
-        ],
-    },
-}
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "fakestoreapi.com",
+      "lh3.googleusercontent.com",
+      "sun1-28.userapi.com",
+      "sun6-22.userapi.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
